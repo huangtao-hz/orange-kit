109e6c072a90590fb736b98f627ceb2475562d30
huangtao-hz <huangtao.sh@icloud.com>
2026-02-25 13:59:35 +0800

    upgrade

--- src/orange/__version__.py
+++ src/orange/__version__.py
@@ -1,1 +1,1 @@
-version = "0.1.9b165"
+version = "0.1.9b166"
--- src/orange/xlwt/__init__.py
+++ src/orange/xlwt/__init__.py
@@ -1,3 +1,5 @@
 # pylint:disable=unused-import
 from .xlwt import Book as Book
 from .xlwt import Sheet as Sheet
+
+__all__ = ["Book", "Sheet"]
